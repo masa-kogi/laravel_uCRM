7cc18686e093d9621853ed75bbe5e69f5c7e90e0
masa-kogi <mk54325@yahoo.co.jp>
2022-10-25 13:57:25 +0000

    redirect to login

--- routes/web.php
+++ routes/web.php
@@ -46,14 +46,16 @@ Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('
 Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('inertia.delete');
 
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
